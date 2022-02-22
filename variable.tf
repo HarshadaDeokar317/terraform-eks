66d0414399c779c8e1dc57a4d45cd2a484693a0e
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2022-02-22 11:45:22 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY" {
+  default = "AKIA2M3VIEP5XUSDSDO6"
+}
+
+variable "AWS_SECRET_KEY" {
+  default = "D5a4QK471hQmdsoeKMKDvrnazqlncKygHepy7Jh1"
+}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
